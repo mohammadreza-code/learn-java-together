89bf7d2ad139239abe4656b48dcf8bc4d6eff4e0
JavaBaz <Bazgir.civil@gmail.com>
2025-03-23 15:51:11 +0330

    add sample Calculator

---
+++ week01/src/main/java/com/github/javabaz/Calculator.java
@@ -0,0 +1,25 @@
+package com.github.javabaz;
+
+public class Calculator {
+
+    // NOTE : Don't edit this file!;
+    // copy past this file and rename new file with your own code!
+
+
+    public int add(int a, int b) {
+        // todo
+    }
+
+    public int subtract(int a, int b) {
+        // todo
+    }
+
+    public int multiply(int a, int b) {
+        // todo
+    }
+
+    public int divide(int a, int b) {
+        // todo
+    }
+}
+
